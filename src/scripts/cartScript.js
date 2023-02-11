2435fda082cfb008e3f0899c106b5101a47887bd
LeoHabs <117355219+LeoHabs@users.noreply.github.com>
2023-02-11 16:30:18 +0000

    small fix to cartscript
    Fixed small bug on on checkout cart images

--- src/scripts/cartScript.js
+++ src/scripts/cartScript.js
@@ -4,40 +4,40 @@ const inventoryBtn = document.getElementById("inventoryBtn");
 let timeToExpire = 86400000;
 let inventory = [];
 
-class StorageObject{
-    constructor(payload,expireDate){
+class StorageObject {
+    constructor(payload, expireDate) {
         this.payload = payload;
         this.expireDate = expireDate;
     }
 };
 
 class Item {
-    constructor(imageSrc,nameOfItem,price){
+    constructor(imageSrc, nameOfItem, price) {
         this.imageSrc = imageSrc;
         this.nameOfItem = nameOfItem;
         this.price = price;
     }
 };
 
-const quackman = new Item("/storeImgs/0B34F818-81BD-4EEC-90BF-A01F47B8AC57.jpeg","QuackMan","17.99$");
-const quacktopus = new Item ("/storeImgs/63d35d04fbc549b7a482b7eb45ba3ca7xl.webp","Quacktopus","18.99$");
-const classyDuck = new Item ("/storeImgs/23206NEW_grande.webp","ClassyDuck","4.99$");
-const quickit = new Item ("/storeImgs/31001_white_hot_safety_bath_ducky.jpeg","Quickit","17.99$");
-const quackuaman = new Item ("/storeImgs/dc-comics-paladone-aquaman-rubber-duck.jpeg","Quackuaman","5.99$");
-const quackespeare = new Item ("/storeImgs/IMG_0524.jpeg","Quackespeare","6.99$");
-const duckyliberty = new Item ("/storeImgs/IMG_5491__88038.jpeg","Ducky Liberty","8.99$");
-const duckyoda = new Item ("/storeImgs/joda-rubber-duck.jpg.webp","Duckyoda","18.99$");
+const quackman = new Item("/src/storeImgs/0B34F818-81BD-4EEC-90BF-A01F47B8AC57.jpeg", "QuackMan", "17.99$");
+const quacktopus = new Item("/src/storeImgs/23206NEW_grande.webp", "Quacktopus", "18.99$");
+const classyDuck = new Item("/src/storeImgs/31001_white_hot_safety_bath_ducky.jpeg", "ClassyDuck", "4.99$");
+const quickit = new Item("/src/storeImgs/63d35d04fbc549b7a482b7eb45ba3ca7xl.webp", "Quickit", "17.99$");
+const quackuaman = new Item("/src/storeImgs/dc-comics-paladone-aquaman-rubber-duck.jpeg", "Quackuaman", "5.99$");
+const quackespeare = new Item("/src/storeImgs/IMG_0524.jpeg", "Quackespeare", "6.99$");
+const duckyliberty = new Item("/src/storeImgs/IMG_5491__88038.jpeg", "Ducky Liberty", "8.99$");
+const duckyoda = new Item("/src/storeImgs/joda-rubber-duck.jpg.webp", "Duckyoda", "18.99$");
 const duckStorage = [];
 
-window.addEventListener("load",()=>{
-    duckStorage.push(quackman,quacktopus,classyDuck,quickit,quackuaman,quackespeare,duckyliberty,duckyoda);
+window.addEventListener("load", () => {
+    duckStorage.push(quackman, quacktopus, classyDuck, quickit, quackuaman, quackespeare, duckyliberty, duckyoda);
 });
 
-inventoryBtn.addEventListener("click",()=>{
+inventoryBtn.addEventListener("click", () => {
     generateInventory();
 });
 
-function generateInventory(){
+function generateInventory() {
     inventory = localStorageGetter("inventory");
     userInventory.innerHTML = "";
     const inventoryTitle = document.createElement("h3");
@@ -46,7 +46,7 @@ function generateInventory(){
     for (let i = 0; i < inventory.length; i++) {
         const item = document.createElement("li");
         const imgOfItem = document.createElement("img");
-        imgOfItem.src= inventory[i].imageSrc;
+        imgOfItem.src = inventory[i].imageSrc;
         const descSpan = document.createElement("span");
         const priceSpan = document.createElement("span");
         descSpan.innerHTML = inventory[i].nameOfItem;
@@ -65,30 +65,30 @@ function generateInventory(){
 
 
 for (let i = 0; i < cartBtns.length; i++) {
-    cartBtns[i].addEventListener("click",()=>{
+    cartBtns[i].addEventListener("click", () => {
         const itemForThisBtn = duckStorage[i];
         inventory.push(itemForThisBtn);
-        localStorageSetter("inventory",inventory);
+        localStorageSetter("inventory", inventory);
     });
 };
 
-function localStorageSetter(id,obj){
+function localStorageSetter(id, obj) {
     const date = Date.now() + timeToExpire;
-    const stringifiedObj = JSON.stringify(new StorageObject(obj,date));
-    localStorage.setItem(id,stringifiedObj);
+    const stringifiedObj = JSON.stringify(new StorageObject(obj, date));
+    localStorage.setItem(id, stringifiedObj);
 };
 
-function localStorageGetter(id){
+function localStorageGetter(id) {
     const objComing = JSON.parse(localStorage.getItem(id));
     const currentTime = Date.now();
     let objReturn = null;
-    if(currentTime<objComing.expireDate){
+    if (currentTime < objComing.expireDate) {
         objReturn = objComing.payload;
     }
-   return objReturn;
+    return objReturn;
 };
 
-function localStorageRemover(id){
+function localStorageRemover(id) {
     localStorage.removeItem(id);
 };
 
